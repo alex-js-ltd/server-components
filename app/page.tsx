f1c6deab1421ea30d2ff8db1629a6da333fd1e6b
Alexander Gilbert <alexandergilbert@Alexanders-MBP.fritz.box>
2023-10-06 13:41:13 +0200

    update modals

--- app/page.tsx
+++ app/page.tsx
@@ -14,11 +14,26 @@ export default function Home() {
             type="button"
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
           >
-            Default
+            Login
           </button>
         </Modal.OpenButton>
 
-        <Modal.Contents title="hello">
+        <Modal.Contents title="Login">
+          <LoginForm submitButton={<button></button>} />
+        </Modal.Contents>
+      </Modal>
+
+      <Modal>
+        <Modal.OpenButton>
+          <button
+            type="button"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          >
+            Register
+          </button>
+        </Modal.OpenButton>
+
+        <Modal.Contents title="Register">
           <LoginForm submitButton={<button></button>} />
         </Modal.Contents>
       </Modal>
