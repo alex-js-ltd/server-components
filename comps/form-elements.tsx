6a789b964e2d5b2602bccbf39e86cdd9e571fedd
Alexander Gilbert <alexandergilbert@Alexanders-MacBook-Pro.fritz.box>
2023-10-06 16:17:22 +0200

    update styles

--- comps/form-elements.tsx
+++ comps/form-elements.tsx
@@ -1,15 +1,12 @@
-import { clsx } from 'clsx'
+const FormGroup = (props: JSX.IntrinsicElements['input']) => (
+  <div {...props} className="flex flex-col w-full my-2" />
+)
 
-const FormGroup = (props: JSX.IntrinsicElements['input']) => {
-  return <div {...props} className="flex flex-col w-full my-2" />
-}
-
-const Input = (props: JSX.IntrinsicElements['input']) => {
-  const className = clsx(
-    'border border-gray-300 bg-gray-200 p-2 rounded-md',
-    props.className,
-  )
-  return <input {...props} className={className} />
-}
+const Input = (props: JSX.IntrinsicElements['input']) => (
+  <input
+    {...props}
+    className="border border-gray-300 bg-gray-200 p-2 rounded-md"
+  />
+)
 
 export { FormGroup, Input }
