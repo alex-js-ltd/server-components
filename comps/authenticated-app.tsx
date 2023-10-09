039a3070c001a28c9f770e7d876fc74ed6b1e9ec
Alexander Gilbert <alexandergilbert@Alexanders-MacBook-Pro.fritz.box>
2023-10-09 17:03:26 +0200

    update button

--- comps/authenticated-app.tsx
+++ comps/authenticated-app.tsx
@@ -4,6 +4,7 @@ import type { User } from '@clerk/nextjs/api'
 import { Fragment } from 'react'
 import { Button } from '@/comps/buttons'
 import { currentUser } from '@clerk/nextjs'
+import Link from 'next/link'
 
 const AuthenticatedApp = async ({ children }: { children: ReactNode }) => {
   const user: User | null = await currentUser()
@@ -21,9 +22,7 @@ const AuthenticatedApp = async ({ children }: { children: ReactNode }) => {
 
       <div className="mx-auto px-8 max-w-screen-lg w-full grid gap-4 grid-cols-1fr-3fr sm:grid-cols-1 sm:grid-rows-auto sm:w-full">
         <div className="relative">
-          <div>hello</div>
-          <div>hello</div>
-          <div>hello</div>
+          <Nav />
         </div>
 
         <main className="w-full">{children}</main>
@@ -31,5 +30,21 @@ const AuthenticatedApp = async ({ children }: { children: ReactNode }) => {
     </Fragment>
   )
 }
+
+const Nav = () => (
+  <nav className="sticky top-4 px-6 py-4 border border-gray-100 rounded">
+    <ul className="list-none p-0">
+      <li>
+        <Link href="/reading-list">Reading List</Link>
+      </li>
+      <li>
+        <Link href="/finished">Finished Books</Link>
+      </li>
+      <li>
+        <Link href="/discover">Discover</Link>
+      </li>
+    </ul>
+  </nav>
+)
 
 export default AuthenticatedApp
--- comps/buttons.tsx
+++ comps/buttons.tsx
@@ -2,22 +2,25 @@ import { clsx } from 'clsx'
 
 const CircleButton = (props: JSX.IntrinsicElements['button']) => (
   <button
-    className="rounded-full w-10 h-10 leading-10 flex items-center justify-center bg-base text-text border border-gray-10 cursor-pointer"
     {...props}
+    className="rounded-full w-10 h-10 leading-10 flex items-center justify-center bg-base text-text border border-gray-10 cursor-pointer"
   />
 )
 
 const Button = ({
   variant,
   ...rest
-}: JSX.IntrinsicElements['button'] & { variant: 'primary' | 'secondary' }) => (
-  <button
-    className={clsx('px-4 py-2 rounded border', {
-      'bg-indigo-500 text-white': variant === 'primary',
-      'bg-gray-300 text-black': variant === 'secondary',
-    })}
-    {...rest}
-  />
-)
+}: JSX.IntrinsicElements['button'] & { variant: 'primary' | 'secondary' }) => {
+  const { className } = rest
+  return (
+    <button
+      {...rest}
+      className={clsx(className, 'px-4 py-2 rounded border', {
+        'bg-indigo-500 text-white': variant === 'primary',
+        'bg-gray-300 text-black': variant === 'secondary',
+      })}
+    />
+  )
+}
 
 export { CircleButton, Button }
