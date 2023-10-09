f29d197ae8838bca33a544fe4c29b1be770edd04
Alexander Gilbert <alexandergilbert@Alexanders-MBP.fritz.box>
2023-10-09 15:42:30 +0200

    update discover book screen

--- app/discover/page.tsx
+++ app/discover/page.tsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import { Input } from '@/comps/form-elements'
 
 const DisoverBooksScreen = () => {
-  return <div>DisoverBooksScreen</div>
+  const handleSearch = async (data: FormData) => {
+    'use server'
+  }
+  return (
+    <div>
+      <div>
+        <form action={handleSearch}>
+          <Input placeholder="Search books..." id="search" type="search" />
+
+          <label htmlFor="search">
+            <button
+              type="submit"
+              className="border-0 relative ml-[-35px] bg-transparent"
+            ></button>
+          </label>
+        </form>
+      </div>
+      <div></div>
+    </div>
+  )
 }
 
 export default DisoverBooksScreen
