0b1d88396573b0d95e2965ba15a8ae3b9f49f858
Alexander Gilbert <alexandergilbert@Alexanders-MacBook-Pro.fritz.box>
2023-10-12 01:06:19 +0200

    update

--- app/authenticated-app.tsx
+++ app/authenticated-app.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Input } from '@/comps/form-elements'
 import BookList from '@/comps/book-list'
 import { getBooks } from '@/utils/actions'
+import { FaSearch, FaTimes } from 'react-icons/fa'
 
 const AuthenticatedApp = async ({
   searchParams,
@@ -23,7 +24,9 @@ const AuthenticatedApp = async ({
           <button
             type="submit"
             className="border-0 relative ml-[-35px] bg-transparent"
-          ></button>
+          >
+            <FaSearch aria-label="search" />
+          </button>
         </label>
       </form>
 
