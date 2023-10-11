bcf88c5c623458ef11c00645096b12f0e9eb6f94
Alexander Gilbert <alexandergilbert@Alexanders-MacBook-Pro.fritz.box>
2023-10-11 03:22:04 +0200

    update

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,15 @@ import { SignedIn, SignedOut } from '@clerk/nextjs'
 const AuthenticatedApp = lazy(() => import('./authenticated-app'))
 const UnauthenticatedApp = lazy(() => import('./unauthenticated-app'))
 
-const Home = () => (
-  <Suspense>
-    <SignedIn>
-      <AuthenticatedApp />
-    </SignedIn>
-    <SignedOut>
-      <UnauthenticatedApp />
-    </SignedOut>
-  </Suspense>
-)
-
-export default Home
+export default function Home() {
+  return (
+    <Suspense>
+      <SignedIn>
+        <AuthenticatedApp />
+      </SignedIn>
+      <SignedOut>
+        <UnauthenticatedApp />
+      </SignedOut>
+    </Suspense>
+  )
+}
