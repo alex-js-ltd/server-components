79de57e0f389a4e213ce5abb4f8b31e5285f0a75
Alexander Gilbert <alexandergilbert@Alexanders-MBP.fritz.box>
2023-10-12 14:58:20 +0200

    update buttons

--- comps/buttons.tsx
+++ comps/buttons.tsx
@@ -7,24 +7,41 @@ import Spinner from './spinner'
 // @ts-expect-error
 import { experimental_useFormStatus as useFormStatus } from 'react-dom'
 
-const CircleButton = (props: JSX.IntrinsicElements['button']) => (
-  <button
-    {...props}
-    className="rounded-full w-10 h-10 leading-10 flex items-center justify-center bg-base text-text border border-gray-10 cursor-pointer bg-white"
-  />
-)
+type CircleButtonProps = JSX.IntrinsicElements['button'] & {
+  variant?: 'indigo' | 'red' | 'white' | 'gray' | 'green'
+}
 
-const Button = ({
-  variant,
-  ...rest
-}: JSX.IntrinsicElements['button'] & { variant: 'primary' | 'secondary' }) => {
-  const { className } = rest
+const CircleButton = (props: CircleButtonProps) => {
+  const { className, variant, ...rest } = props
+  return (
+    <button
+      {...rest}
+      className={clsx(
+        className,
+        'rounded-full w-10 h-10 leading-10 flex items-center justify-center bg-base text-text border border-gray-10 cursor-pointer bg-white',
+
+        {
+          'hover:text-indigo-300': variant === 'indigo',
+          'hover:text-red-300': variant === 'red',
+          'hover:text-white': variant === 'white',
+          'hover:text-gray-300': variant === 'gray',
+          'hover:text-green-300': variant === 'green',
+        },
+      )}
+    />
+  )
+}
+
+const Button = (
+  props: JSX.IntrinsicElements['button'] & { variant: 'primary' | 'secondary' },
+) => {
+  const { className, variant, ...rest } = props
 
   return (
     <button
       {...rest}
       className={clsx(className, 'px-4 py-2 rounded border', {
-        'bg-indigo-500 text-white': variant === 'primary',
+        'bg-indigo-300 text-white': variant === 'primary',
         'bg-gray-300 text-black': variant === 'secondary',
       })}
     />
@@ -47,14 +64,26 @@ const SignOutButton = () => {
   )
 }
 
-const SubmitButton = ({ icon }: { icon: ReactElement }) => {
+const SubmitButton = ({
+  variant,
+  icon,
+}: {
+  variant: CircleButtonProps['variant']
+  icon: ReactElement
+}) => {
   const { pending } = useFormStatus()
 
   return (
-    <CircleButton type="submit" aria-disabled={pending}>
+    <CircleButton
+      variant={pending ? 'gray' : variant}
+      type="submit"
+      aria-disabled={pending}
+    >
       {pending ? <Spinner /> : icon}
     </CircleButton>
   )
 }
 
 export { CircleButton, Button, SignOutButton, SubmitButton }
+
+export type { CircleButtonProps }
--- comps/status-buttons.tsx
+++ comps/status-buttons.tsx
@@ -1,5 +1,6 @@
 import type { Book } from '@prisma/client'
 import type { ReactElement } from 'react'
+import type { CircleButtonProps } from './buttons'
 import { Fragment } from 'react'
 import {
   FaCheckCircle,
@@ -15,15 +16,17 @@ const TooltipButton = ({
   action,
   book,
   icon,
+  variant,
 }: {
   action: (book: Book) => Promise<void>
   book: Book
   icon: ReactElement
+  variant?: CircleButtonProps['variant']
 }) => {
   const actionWithArgument = action.bind(null, book)
   return (
     <form action={actionWithArgument}>
-      <SubmitButton icon={icon} />
+      <SubmitButton variant={variant} icon={icon} />
     </form>
   )
 }
@@ -44,6 +47,7 @@ const StatusButtons = async ({ book }: { book: Book }) => {
             action={actions.removeListItem}
             book={listItem}
             icon={<FaCheckCircle />}
+            variant="green"
           />
         )
       ) : null}
@@ -53,12 +57,14 @@ const StatusButtons = async ({ book }: { book: Book }) => {
           action={actions.removeListItem}
           book={listItem}
           icon={<FaMinusCircle />}
+          variant="red"
         />
       ) : (
         <TooltipButton
           action={actions.createListItem}
           book={book}
           icon={<FaPlusCircle />}
+          variant="indigo"
         />
       )}
     </Fragment>
