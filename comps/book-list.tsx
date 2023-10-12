797ff05bf33ad348a6a9703b85e1341a8d77bfc8
Alexander Gilbert <alexandergilbert@Alexanders-MBP.fritz.box>
2023-10-12 12:46:50 +0200

    update

--- comps/book-list.tsx
+++ comps/book-list.tsx
@@ -1,7 +1,7 @@
-import type { Book, ListItem } from '@prisma/client'
+import type { Book } from '@prisma/client'
 import BookRow from './book-row'
 
-const BookList = ({ books }: { books: Array<Book | ListItem> }) => (
+const BookList = ({ books }: { books: Array<Book> }) => (
   <ul className="list-none p-0 grid grid-rows-auto-100 gap-4 mt-5">
     {books.map(book => (
       <li key={book.id}>
