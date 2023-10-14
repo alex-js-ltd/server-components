1f24c9039a7564dd47f886b490e8e2813ce6f981
Alexander Gilbert <alexandergilbert@Alexanders-MBP.fritz.box>
2023-10-14 13:32:41 +0200

    update suspense

--- app/discover/page.tsx
+++ app/discover/page.tsx
@@ -1,8 +1,11 @@
-import SearchForm from '@/comps/search-form'
+import { Suspense } from 'react'
+import { Input } from '@/comps/form-elements'
+import { FaSearch } from 'react-icons/fa'
 import BookList from '@/comps/book-list'
+import Loading from '@/comps/loading'
 import * as actions from '@/utils/actions'
 
-const Page = async ({
+const Page = ({
   searchParams,
 }: {
   searchParams: { [key: string]: string | string[] | undefined }
@@ -11,15 +14,37 @@ const Page = async ({
 
   const startsWith = query?.toString() ?? ''
 
-  const books = await actions.getBooks(startsWith)
-
   return (
     <div>
-      <SearchForm defaultValue={startsWith} />
+      <form className="mb-5">
+        <Input
+          placeholder="Search books..."
+          name="query"
+          className="w-full"
+          defaultValue={startsWith}
+        />
 
-      <BookList books={books} />
+        <label htmlFor="search">
+          <button
+            type="submit"
+            className="border-0 relative ml-[-35px] bg-transparent"
+          >
+            <FaSearch aria-label="search" />
+          </button>
+        </label>
+      </form>
+
+      <Suspense fallback={<Loading />}>
+        <DiscoverList startsWith={startsWith} />
+      </Suspense>
     </div>
   )
 }
 
 export default Page
+
+const DiscoverList = async ({ startsWith }: { startsWith: string }) => {
+  const books = await actions.getBooks(startsWith)
+
+  return <BookList books={books} />
+}
